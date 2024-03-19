f9442abfb612f9a2c756c53dbe5ebcf0b936617c
seymanurks <nurks1905@gmail.com>
2024-03-19 17:03:31 +0300

    tab navigation structure

---
+++ src/Router.tsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Favorites from './pages/Favorites';
+import Products from './pages/Products';
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+return(
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Products" component={Products}></Tab.Screen>
+      <Tab.Screen name="Favorites" component={Favorites}></Tab.Screen>
+    </Tab.Navigator>
+  </NavigationContainer>
+  )
+}
+
+export default App;
